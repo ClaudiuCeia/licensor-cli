a5e09eb01edd5ab6a4bc5a112bad6f7c465c2a34
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-01 00:48:37 +0300

    :rocket:

---
+++ src/cli.js
@@ -0,0 +1,206 @@
+'use strict';
+
+const inquirer = require('inquirer');
+const licenses = require('./licenses');
+const stripIndent = require('strip-indent');
+const chalk = require('chalk');
+const fs = require('fs');
+
+console.log('Hi, let\'s choose the right license for your project!\n\n');
+
+var questions = [
+  {
+    type: 'confirm',
+    name: 'isOpenSource',
+    message: 'Is this an Open Source project?',
+    default: true
+  },
+  {
+    type: 'list',
+    name: 'permisivness',
+    message: 'Which of these statements best describes your situation?',
+    choices: [
+      {
+        name: 'I want a simple, permissive license',
+        value: 'permissive',
+        short: 'Permissive',
+      },
+      {
+        name: 'I\'m worried about patents or other rights',
+        value: 'patents',
+        short: 'Patents',
+      },
+      {
+        name: 'I would like people that use my code to release it under the same terms',
+        value: 'copyleft',
+        short: 'Copyleft',
+      },
+      {
+        name: 'I want my project to be commercially-friendly',
+        value: 'commercially-friendly',
+        short: 'Commercially-friendly',
+      },
+      {
+        name: 'Huh? I don\'t really care about any of these. Just let people use it.',
+        value: 'public-domain',
+        short: 'Public domain',
+      },
+    ],
+    when: function(answers) {
+      return answers.isOpenSource;
+    },
+  },
+  {
+    type: 'list',
+    name: 'licenseType',
+    message: (answers) => {
+      let licenseType = answers.permisivness;
+
+      if (answers.permisivness === 'patents') {
+        licenseType = 'permissive';
+      }
+
+      return `Here are a few ${licenseType} licenses for you to choose from.`;
+    },
+    choices: (answers) => {
+      const suitableLicenses = licenses.filter((license) => {
+        return license.type === answers.permisivness;
+      });
+
+      return suitableLicenses.map((license) => {
+        return {
+          name: license.name,
+          value: license,
+          short: license.name,
+        };
+      });
+    },
+    when: (answers) => {
+      return answers.isOpenSource;
+    },
+  },
+  /* {
+    type: 'confirm',
+    name: 'seeDescription',
+    message: (answers) => {
+      return chalk.bold('Sounds pretty good!') + chalk.gray(answers.licenseType.description);
+    },
+    default: true
+  }, */
+  {
+    type: 'input',
+    name: 'year',
+    message: 'When did you start working on this project (what year?)',
+    validate: (input) => {
+      if (input < 1950 || input > (new Date()).getFullYear()) {
+        return 'Is this the source code for the time machine?';
+      }
+
+      return true;
+    },
+    when: (answers) => {
+      const hasParam = answers.licenseType.params.indexOf('year') !== -1;
+      return answers.isOpenSource && hasParam;
+    },
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'And your name is...?',
+    when: (answers) => {
+      const hasParam = answers.licenseType.params.indexOf('name') !== -1;
+      return answers.isOpenSource && hasParam;
+    },
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Tell us a couple of words about your project',
+    when: (answers) => {
+      const hasParam = answers.licenseType.params.indexOf('description') !== -1;
+      return answers.isOpenSource && hasParam;
+    },
+  },
+  {
+    type: 'list',
+    name: 'saveCheck',
+    message: 'Final step! What do you want to do with the license?',
+    choices: () => {
+      let choices = [{
+        name: 'Show in stdout',
+        value: 'stdout',
+        short: 'stdout',
+      }];
+
+      if (!fs.existsSync('./LICENSE')) {
+        choices.push({
+          name: 'Save content as LICENSE',
+          value: 'create',
+          short: 'New LICENSE'
+        });
+      } else {
+        choices.push({
+          name: 'Save content as LICENSE_NEW (LICENSE file already exists)',
+          value: 'create_secondary',
+          short: 'New LICENSE_NEW'
+        });
+        choices.push({
+          name: 'Save content as LICENSE and rename existing file to LICENSE.bak',
+          value: 'create_secondary_and_rename',
+          short: 'New LICENSE and rename old'
+        });
+      }
+
+      return choices;
+    }
+  }
+];
+
+inquirer.prompt(questions).then(answers => {
+  let params = {};
+  let chosenLicense = {};
+  for (const license of licenses) {
+    if (license.name !== answers.licenseType.name) {
+      continue;
+    }
+
+    chosenLicense = answers.licenseType;
+    params = chosenLicense.params.reduce((acc, param) => {
+      acc = {
+        ...acc,
+        [param]: answers[param]
+      };
+
+      return acc;
+    }, {});
+
+    break;
+  }
+
+  const licenseText = stripIndent(chosenLicense.license(params));
+
+  if (answers.saveCheck === 'stdoud') {
+    console.log(licenseText);
+  }
+
+  let filePath = './LICENSE';
+  if (answers.saveCheck === 'create_secondary') {
+    filePath += '_NEW';
+  }
+
+  if (answers.saveCheck === 'create_secondary_and_rename') {
+    fs.rename('./LICENSE', './LICENSE.bak', (err) => {
+      if (err) {
+        return console.warn(err);
+      }
+    });
+  }
+
+  fs.writeFile(filePath, licenseText, function(err) {
+    if(err) {
+      return console.warn(err);
+    }
+
+    console.log('All done!');
+  });
+});
---
+++ src/licenses.js
@@ -0,0 +1,146 @@
+module.exports = [{
+  name: 'MIT License',
+  license: require('./licenses/MITLicense'),
+  params: ['name', 'year'],
+  type: 'permissive',
+  description: `
+    A short and simple permissive license with conditions only
+    requiring preservation of copyright and license notices. Licensed works,
+    modifications, and larger works may be distributed under different terms
+    and without source code.
+  `,
+},
+{
+  name: 'Apache License 2.0',
+  license: require('./licenses/ApacheLicense2'),
+  params: ['name', 'year'],
+  type: 'patents',
+  description: `
+    A permissive license whose main conditions require preservation of
+    copyright and license notices. Contributors provide an express grant of
+    patent rights. Licensed works, modifications, and larger works may be
+    distributed under different terms and without source code.
+  `
+},
+{
+  name: 'GNU General Public License v3.0',
+  license: require('./licenses/GNUGeneralPublicLicense3'),
+  params: ['name', 'year', 'description'],
+  type: 'copyleft',
+  description: `
+    Permissions of this strong copyleft license are conditioned on making
+    available complete source code of licensed works and modifications, which
+    include larger works using a licensed work, under the same license.
+    Copyright and license notices must be preserved. Contributors provide an
+    express grant of patent rights
+  `
+},
+{
+  name: 'BSD 2-Clause "Simplified" License',
+  license: require('./licenses/BSD2Simplified'),
+  params: ['year', 'name'],
+  type: 'permissive',
+  description: `
+    A permissive license that comes in two variants, the BSD 2-Clause and
+    BSD 3-Clause. Both have very minute differences to the MIT license.
+  `
+},
+{
+  name: 'BSD 3-Clause "New" or "Revised" License',
+  license: require('./licenses/BSD3ClauseNewOrRevisedLicense'),
+  type: 'permissive',
+  params: ['year', 'name'],
+  description: `
+    A permissive license that comes in two variants, the BSD 2-Clause and
+    BSD 3-Clause. Both have very minute differences to the MIT license.
+  `
+},
+{
+  name: 'Eclipse Public License 2.0',
+  license: require('./licenses/EclipsePublicLicense2'),
+  type: 'commercially-friendly',
+  params: [],
+  description: `
+    This commercially-friendly copyleft license provides the ability to
+    commercially license binaries; a modern royalty-free patent license
+    grant; and the ability for linked works to use other licenses, including
+    commercial ones
+  `
+},
+{
+  name: 'GNU Affero General Public License 3.0',
+  license: require('./licenses/GNUAfferoGeneralPublicLicense3'),
+  type: 'copyleft',
+  params: ['year', 'name', 'description'],
+  description: `
+    Permissions of this strongest copyleft license are conditioned on making
+    available complete source code of licensed works and modifications, which
+    include larger works using a licensed work, under the same license.
+    Copyright and license notices must be preserved. Contributors provide an
+    express grant of patent rights. When a modified version is used to provide
+    a service over a network, the complete source code of the modified version
+    must be made available.
+  `
+},
+{
+  name: 'GNU General Public License v2.0',
+  license: require('./licenses/GNUGeneralPublicLicense2'),
+  params: ['year', 'name', 'description'],
+  type: 'copyleft',
+  description: `
+  The GNU GPL is the most widely used free software license and has a strong
+  copyleft requirement. When distributing derived works, the source code of the
+  work must be made available under the same license. There are multiple
+  variants of the GNU GPL, each with different requirements.
+  `
+},
+{
+  name: 'GNU Lesser General Public License v2.1',
+  license: require('./licenses/GNULesserGeneralPublicLicense2_1'),
+  params: ['year', 'name', 'description'],
+  type: 'copyleft',
+  description: `
+  Primarily used for software libraries, the GNU LGPL requires that derived
+  works be licensed under the same license, but works that only link to it do
+  not fall under this restriction. There are two commonly used versions of the
+  GNU LGPL.
+  `
+},
+{
+  name: 'GNU Lesser General Public License v3.0',
+  license: require('./licenses/GNULesserGeneralPublicLicense3'),
+  params: ['year', 'name', 'description'],
+  type: 'copyleft',
+  description: `
+  Permissions of this strong copyleft license are conditioned on making available
+  complete source code of licensed works and modifications, which include larger
+  works using a licensed work, under the same license. Copyright and license
+  notices must be preserved. Contributors provide an express grant of patent
+  rights  `
+},
+{
+  name: 'Mozilla Public License 2.0',
+  license: require('./licenses/MozillaPublicLicense2'),
+  params: [],
+  type: 'copyleft',
+  description: `
+    Permissions of this weak copyleft license are conditioned on making
+    available source code of licensed files and modifications of those files
+    under the same license (or in certain cases, one of the GNU licenses).
+    Copyright and license notices must be preserved. Contributors provide an
+    express grant of patent rights. However, a larger work using the licensed
+    work may be distributed under different terms and without source code for
+    files added in the larger work
+  `
+},
+{
+  name: 'The Unlicense',
+  license: require('./licenses/TheUnlicense'),
+  params: [],
+  type: 'public-domain',
+  description: `
+    A license with no conditions whatsoever which dedicates works to the public
+    domain. Unlicensed works, modifications, and larger works may be distributed
+    under different terms and without source code
+  `
+}];
